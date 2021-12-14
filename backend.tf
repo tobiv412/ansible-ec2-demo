3d31055fed29d83b36fc2d4b50a6b66b7d09794f
tobiv412 <victor.tobi@cloudreach.com>
2021-12-14 20:55:22 +0000

    dynamodb

--- backend.tf
+++ backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket         = "talent-academy-412-tfstates-vic"
     key            = "Week-4/Ansible/ansible-ec2-demo/terraform.tfstates"
-    dynamodb_table = "terraform-lock"
+    # dynamodb_table = "terraform-lock" just to bypass 
   }
 }
